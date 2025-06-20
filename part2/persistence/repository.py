f45450f86802579238059266a71dc7ee1e0b6a69
HectorPR4546 <h_perez93@yahoo.com>
2025-06-19 20:30:32 -0400

    Task 1 Updated Code

--- part2/persistence/repository.py
+++ part2/persistence/repository.py
@@ -1,23 +1,35 @@
 """Repository interface definition."""
 
 from abc import ABC, abstractmethod
-from typing import Dict, Any
+from typing import Dict, Any, Type, TypeVar
 from models.base_model import BaseModel
+
+T = TypeVar('T', bound=BaseModel)
 
 class Repository(ABC):
     """Abstract base class for repository implementations."""
     
     @abstractmethod
-    def create(self, obj_data: Dict[str, Any]) -> BaseModel:
-        """Create a new object from data."""
+    def create(self, obj: T) -> T:
+        """Create a new object."""
         pass
     
     @abstractmethod
-    def get(self, obj_id: str) -> BaseModel:
+    def get(self, model_class: Type[T], obj_id: str) -> T:
         """Get an object by ID."""
         pass
     
     @abstractmethod
-    def get_all(self) -> Dict[str, BaseModel]:
-        """Get all objects."""
+    def get_all(self, model_class: Type[T]) -> Dict[str, T]:
+        """Get all objects of a specific model class."""
+        pass
+    
+    @abstractmethod
+    def update(self, obj: T) -> T:
+        """Update an existing object."""
+        pass
+    
+    @abstractmethod
+    def delete(self, model_class: Type[T], obj_id: str) -> bool:
+        """Delete an object by ID."""
         pass
