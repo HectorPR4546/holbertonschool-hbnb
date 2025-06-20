dcfb21e0c183bb5bb8702e41af38387f8be8a1e1
HectorPR4546 <h_perez93@yahoo.com>
2025-06-19 20:17:28 -0400

    Task 1 Updated code

--- part2/models/__init__.py
+++ part2/models/__init__.py
@@ -1,2 +1,6 @@
 """Models package initialization."""
 from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
