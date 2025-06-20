24a9e550e4c69d7712d91f9b2b7c05b5ba4e656f
HectorPR4546 <h_perez93@yahoo.com>
2025-06-19 20:32:48 -0400

    Task 1 Updated Code

--- part2/persistence/in_memory_repo.py
+++ part2/persistence/in_memory_repo.py
@@ -1,26 +1,45 @@
 """In-memory repository implementation."""
 
-from typing import Dict, Any
+from typing import Dict, Any, Type, TypeVar
 from models.base_model import BaseModel
 from persistence.repository import Repository
+
+T = TypeVar('T', bound=BaseModel)
 
 class InMemoryRepository(Repository):
     """In-memory implementation of the Repository interface."""
     
     def __init__(self):
         """Initialize the repository."""
-        self._objects: Dict[str, BaseModel] = {}
+        self._objects: Dict[Type, Dict[str, BaseModel]] = {}
     
-    def create(self, obj_data: Dict[str, Any]) -> BaseModel:
-        """Create a new object from data."""
-        obj = BaseModel(**obj_data)
-        self._objects[obj.id] = obj
+    def create(self, obj: T) -> T:
+        """Create a new object."""
+        if obj.__class__ not in self._objects:
+            self._objects[obj.__class__] = {}
+        self._objects[obj.__class__][obj.id] = obj
         return obj
     
-    def get(self, obj_id: str) -> BaseModel:
+    def get(self, model_class: Type[T], obj_id: str) -> T:
         """Get an object by ID."""
-        return self._objects.get(obj_id)
+        if model_class in self._objects:
+            return self._objects[model_class].get(obj_id)
+        return None
     
-    def get_all(self) -> Dict[str, BaseModel]:
-        """Get all objects."""
-        return self._objects.copy()
+    def get_all(self, model_class: Type[T]) -> Dict[str, T]:
+        """Get all objects of a specific model class."""
+        return self._objects.get(model_class, {}).copy()
+    
+    def update(self, obj: T) -> T:
+        """Update an existing object."""
+        if obj.__class__ in self._objects and obj.id in self._objects[obj.__class__]:
+            self._objects[obj.__class__][obj.id] = obj
+            return obj
+        return None
+    
+    def delete(self, model_class: Type[T], obj_id: str) -> bool:
+        """Delete an object by ID."""
+        if model_class in self._objects and obj_id in self._objects[model_class]:
+            del self._objects[model_class][obj_id]
+            return True
+        return False
