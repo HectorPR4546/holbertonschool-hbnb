3cfee8f74b669012e7ca6a8500141877fc51221e
HectorPR4546 <h_perez93@yahoo.com>
2025-06-21 16:55:23 -0400

    Task 3

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
+from app.api.v1.amenities import api as amenities_ns
 
 def create_app():
     """Create and configure the Flask app"""
@@ -13,5 +14,6 @@ def create_app():
 
     # Register namespaces
     api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(amenities_ns, path='/api/v1/amenities')
 
     return app
