10c07a1bdf56e342f43047f4a36eb8c1d97cc28a
HectorPR4546 <h_perez93@yahoo.com>
2025-06-21 14:54:43 -0400

    Task 3 Updated code

--- part2/app/api/v1/__init__.py
+++ part2/app/api/v1/__init__.py
@@ -2,8 +2,8 @@
 """Initialize API v1 package."""
 from flask_restx import Api
 
-# NEW: Added API version and description metadata
 api = Api(version='1.0', title='HBnB API',
           description='A simple HBnB REST API')
 
-from app.api.v1 import users
+# UPDATED: Added amenities to imports
+from app.api.v1 import users, amenities
