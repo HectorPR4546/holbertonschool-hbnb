6577816733c2b00997177795f59b3155ff477402
HectorPR4546 <h_perez93@yahoo.com>
2025-06-22 19:05:32 -0400

    Change of plans

--- part2/app/models/amenity.py
+++
@@ -1,9 +0,0 @@
-from app.models.base_model import BaseModel
-
-class Amenity(BaseModel):
-    def __init__(self, name):
-        super().__init__()
-        self.name = name
-
-    def __repr__(self):
-        return f"<Amenity {self.id} ({self.name})>"
