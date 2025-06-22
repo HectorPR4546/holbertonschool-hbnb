00d61414d6deb2edc360c080685f4f89ebd5203e
HectorPR4546 <h_perez93@yahoo.com>
2025-06-22 14:07:12 -0400

    Updated_Code

--- part2/app/persistence/repository.py
+++ part2/app/persistence/repository.py
@@ -1,59 +1,54 @@
 from abc import ABC, abstractmethod
 
 class Repository(ABC):
-    """Abstract base class for repositories"""
     @abstractmethod
-    def add(self, obj):
-        pass
-
+    def add(self, obj): pass
+    
     @abstractmethod
-    def get(self, obj_id):
-        pass
-
+    def get(self, obj_id): pass
+    
     @abstractmethod
-    def get_all(self):
-        pass
-
+    def get_all(self): pass
+    
     @abstractmethod
-    def update(self, obj_id, data):
-        pass
-
+    def update(self, obj_id, data): pass
+    
     @abstractmethod
-    def delete(self, obj_id):
-        pass
+    def delete(self, obj_id): pass
+    
+    @abstractmethod
+    def get_by_attribute(self, attr_name, attr_value): pass
 
 
 class InMemoryRepository(Repository):
-    """In-memory implementation of Repository"""
     def __init__(self):
         self._storage = {}
 
     def add(self, obj):
-        """Adds an object to storage"""
         self._storage[obj.id] = obj
 
     def get(self, obj_id):
-        """Gets an object by id"""
         return self._storage.get(obj_id)
 
     def get_all(self):
-        """Gets all objects"""
         return list(self._storage.values())
 
     def update(self, obj_id, data):
-        """Updates an object"""
         obj = self.get(obj_id)
         if obj:
             for key, value in data.items():
-                setattr(obj, key, value)
+                if hasattr(obj, key):
+                    setattr(obj, key, value)
+            return obj
+        return None
 
     def delete(self, obj_id):
-        """Deletes an object"""
         if obj_id in self._storage:
             del self._storage[obj_id]
+            return True
+        return False
 
     def get_by_attribute(self, attr_name, attr_value):
-        """Get object by attribute value"""
         for obj in self._storage.values():
             if getattr(obj, attr_name, None) == attr_value:
                 return obj
