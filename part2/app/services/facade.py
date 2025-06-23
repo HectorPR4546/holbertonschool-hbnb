ecd77139cdc06841326dd40c61051e01313c5aef
HectorPR4546 <h_perez93@yahoo.com>
2025-06-23 03:32:40 -0400

    Task 3

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,5 +1,8 @@
 from app.persistence.repository import InMemoryRepository
 from app.models.user import User
+from app.models.place import Place
+from app.models.review import Review
+from app.models.amenity import Amenity
 
 class HBnBFacade:
     def __init__(self):
@@ -8,31 +11,46 @@ class HBnBFacade:
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
+    # --- User methods ---
     def create_user(self, user_data):
-        """Create a new user and store it in the repository"""
         user = User(**user_data)
         self.user_repo.add(user)
         return user
 
     def get_user(self, user_id):
-        """Retrieve a user by ID"""
         return self.user_repo.get(user_id)
 
     def get_user_by_email(self, email):
-        """Retrieve a user by email"""
         return self.user_repo.get_by_attribute('email', email)
 
     def get_all_users(self):
-        """Retrieve all users"""
         return self.user_repo.get_all()
 
     def update_user(self, user_id, user_data):
-        """Update an existing user"""
-        user = self.get_user(user_id)
-        if user:
-            user.update(user_data)
-        return user
+        user = self.user_repo.get(user_id)
+        if not user:
+            return None
+        self.user_repo.update(user_id, user_data)
+        return self.user_repo.get(user_id)
 
-    def get_place(self, place_id):
-        # Will be implemented later
-        pass
+    # --- Amenity methods ---
+    def create_amenity(self, amenity_data):
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+
+    def get_amenity_by_name(self, name):
+        return self.amenity_repo.get_by_attribute('name', name)
+
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        amenity = self.amenity_repo.get(amenity_id)
+        if not amenity:
+            return None
+        self.amenity_repo.update(amenity_id, amenity_data)
+        return self.amenity_repo.get(amenity_id)
