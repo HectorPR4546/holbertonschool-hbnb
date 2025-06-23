645ec51ab0a6a4c0bc91e6d7f609b941af396b6d
HectorPR4546 <h_perez93@yahoo.com>
2025-06-23 03:59:18 -0400

    Fix

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -11,10 +11,39 @@ class HBnBFacade:
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
-    # -- User methods --
+    def get_user_by_email(self, email):
+        """Find a user by email"""
+        return self.user_repo.get_by_attribute('email', email)
+
+    def create_user(self, user_data):
+        user = User(**user_data)
+        self.user_repo.add(user)
+        return user
+
+    def get_user(self, user_id):
+        return self.user_repo.get(user_id)
+
+    def create_amenity(self, amenity_data):
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        amenity = self.get_amenity(amenity_id)
+        if not amenity:
+            return None
+        if 'name' in amenity_data:
+            amenity.name = amenity_data['name']
+        amenity.save()
+        return amenity
 
     def create_place(self, place_data):
-        """Create a new place with validation and store it."""
         place = Place(
             title=place_data['title'],
             description=place_data.get('description', ''),
@@ -40,8 +69,7 @@ class HBnBFacade:
         place = self.get_place(place_id)
         if not place:
             return None
-        valid_keys = ['title', 'description', 'price', 'latitude', 'longitude']
-        for key in valid_keys:
+        for key in ['title', 'description', 'price', 'latitude', 'longitude']:
             if key in place_data:
                 setattr(place, key, place_data[key])
         place.save()
