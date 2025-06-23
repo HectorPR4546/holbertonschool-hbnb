891eeb1c1274e0a7b84cec326211af125ffd4d05
HectorPR4546 <h_perez93@yahoo.com>
2025-06-23 03:51:19 -0400

    Task 4

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -11,46 +11,38 @@ class HBnBFacade:
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
-    # --- User methods ---
-    def create_user(self, user_data):
-        user = User(**user_data)
-        self.user_repo.add(user)
-        return user
+    # -- User methods --
 
-    def get_user(self, user_id):
-        return self.user_repo.get(user_id)
+    def create_place(self, place_data):
+        """Create a new place with validation and store it."""
+        place = Place(
+            title=place_data['title'],
+            description=place_data.get('description', ''),
+            price=place_data['price'],
+            latitude=place_data['latitude'],
+            longitude=place_data['longitude'],
+            owner=self.get_user(place_data['owner_id'])
+        )
+        for amenity_id in place_data.get('amenities', []):
+            am = self.get_amenity(amenity_id)
+            if am:
+                place.add_amenity(am)
+        self.place_repo.add(place)
+        return place
 
-    def get_user_by_email(self, email):
-        return self.user_repo.get_by_attribute('email', email)
+    def get_place(self, place_id):
+        return self.place_repo.get(place_id)
 
-    def get_all_users(self):
-        return self.user_repo.get_all()
+    def get_all_places(self):
+        return self.place_repo.get_all()
 
-    def update_user(self, user_id, user_data):
-        user = self.user_repo.get(user_id)
-        if not user:
+    def update_place(self, place_id, place_data):
+        place = self.get_place(place_id)
+        if not place:
             return None
-        self.user_repo.update(user_id, user_data)
-        return self.user_repo.get(user_id)
-
-    # --- Amenity methods ---
-    def create_amenity(self, amenity_data):
-        amenity = Amenity(**amenity_data)
-        self.amenity_repo.add(amenity)
-        return amenity
-
-    def get_amenity(self, amenity_id):
-        return self.amenity_repo.get(amenity_id)
-
-    def get_amenity_by_name(self, name):
-        return self.amenity_repo.get_by_attribute('name', name)
-
-    def get_all_amenities(self):
-        return self.amenity_repo.get_all()
-
-    def update_amenity(self, amenity_id, amenity_data):
-        amenity = self.amenity_repo.get(amenity_id)
-        if not amenity:
-            return None
-        self.amenity_repo.update(amenity_id, amenity_data)
-        return self.amenity_repo.get(amenity_id)
+        valid_keys = ['title', 'description', 'price', 'latitude', 'longitude']
+        for key in valid_keys:
+            if key in place_data:
+                setattr(place, key, place_data[key])
+        place.save()
+        return place
