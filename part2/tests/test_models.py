7576b0673bb292faa95f1c02b4e86feed6fd4bea
HectorPR4546 <h_perez93@yahoo.com>
2025-06-23 17:14:24 -0400

    More Fix

--- part2/tests/test_models.py
+++ part2/tests/test_models.py
@@ -1,21 +1,31 @@
+import unittest
 from app.models.user import User
 from app.models.place import Place
-from app.models.review import Review
-from app.models.amenity import Amenity
 
-# Create dummy user
-user = User("John", "Doe", "john@example.com")
+class TestModels(unittest.TestCase):
+    def test_create_user(self):
+        user = User("John", "Doe", "john@example.com")
+        self.assertEqual(user.first_name, "John")
+        self.assertEqual(user.last_name, "Doe")
+        self.assertEqual(user.email, "john@example.com")
 
-# Create dummy place
-place = Place("Nice Spot", "Great view", 150.0, 18.0, -66.0, user.id)
+    def test_create_place(self):
+        user = User("John", "Doe", "john@example.com")
+        place = Place("Nice Spot", "Great view", 150.0, 18.0, -66.0, user.id)
+        self.assertEqual(place.title, "Nice Spot")
+        self.assertEqual(place.owner_id, user.id)
 
-# Create dummy review
-review = Review("Amazing experience", 5, user.id, place.id)
+    def test_place_invalid_price(self):
+        user = User("John", "Doe", "john@example.com")
+        with self.assertRaises(ValueError):
+            Place("Cheap Place", "Bad area", -20, 18.0, -66.0, user.id)
 
-# Create dummy amenity
-amenity = Amenity("Free Parking")
+    def test_place_invalid_latitude(self):
+        user = User("John", "Doe", "john@example.com")
+        with self.assertRaises(ValueError):
+            Place("Wrong Latitude", "Invalid", 100.0, 200.0, -66.0, user.id)
 
-print("User:", user.first_name, user.email)
-print("Place:", place.title, place.owner_id)
-print("Review:", review.text, review.rating)
-print("Amenity:", amenity.name)
+    def test_place_invalid_longitude(self):
+        user = User("John", "Doe", "john@example.com")
+        with self.assertRaises(ValueError):
+            Place("Wrong Longitude", "Invalid", 100.0, 18.0, -200.0, user.id)
